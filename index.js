9e920538ea2b6b8262503ea83fe3e05cffbeff0f
Saumya Srivastava <117256519+SaumyaSrivastava13@users.noreply.github.com>
2024-06-04 22:13:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api", playlistRoutes);
 
 let PORT=8000;
-app.listen(8080, "0.0.0.0", ()=>{
+app.listen(8000, "0.0.0.0", ()=>{
     console.log(`server connected at port at ${PORT}`)
 })
 
